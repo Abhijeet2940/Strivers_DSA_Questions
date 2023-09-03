a702c7bca14d17d79c8b839a94ef15483fce266c
Abhijeet2940 <88205898+Abhijeet2940@users.noreply.github.com>
2023-09-03 23:27:38 +0530

    Add files via upload

---
+++ array/rotateAnarray.cpp
@@ -0,0 +1,34 @@
+// rotate an array 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[]={10,20,30,40,50};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     int temp=arr[0];
+//     for(int i=0;i<n;i++){
+//         arr[i]=arr[i+1];
+//         arr[n]=temp;
+//     }
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={10,20,30,40,50};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=1;i<n;i++){
+        int temp=arr[i];
+        arr[i]=arr[i-1];
+        arr[i-1]=temp;
+        
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
