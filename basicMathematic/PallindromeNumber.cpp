da262393791bd9365001c98fa457ebf5b66db27f
Abhijeet2940 <88205898+Abhijeet2940@users.noreply.github.com>
2023-09-07 23:32:11 +0530

    Add files via upload

---
+++ basicMathematic/PallindromeNumber.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int countDigit(int n){
+    int count=0;
+    while(n>0){
+        n/10;
+count++;
+return count ;
+    }
+}
+int main(){
+    int n=2345432;
+    int b=countDigit(n);
+    int c,d;
+    for(int i=0;i<b;i++){
+        
+        
+    }
+}
