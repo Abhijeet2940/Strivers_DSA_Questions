7edb04d355b69531ef376ae9be42457b761441e4
Abhijeet2940 <88205898+Abhijeet2940@users.noreply.github.com>
2023-09-15 23:28:59 +0530

    Add files via upload

---
+++ hashing/countdistinctinwindow.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int arr[]={8,20,7,30};
+ int n=sizeof(arr)/sizeof(arr[0]);
+ int k=2;
+ for(int i=0;i<n;i++){
+    for(int j=i;j<i+k;j++){
+        if(j<i)
+    }
+ }
+return 0;
+}
