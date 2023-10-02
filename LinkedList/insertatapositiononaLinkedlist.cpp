1a832c51af02cf92e20bc00b8a26f3fbeaee1c6a
Abhijeet Kumar <88205898+Abhijeet2940@users.noreply.github.com>
2023-10-02 13:12:34 +0530

    Add files via upload

---
+++ LinkedList/insertatapositiononaLinkedlist.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+//first make a new node
+//then new node.next  should be the position at next
+//then new node.previous should be before
